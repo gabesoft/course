010525fca6d9b9ea7c23a9357d6168a8544a8d12
Gabriel Adomnicai <gabesoft@gmail.com>
2016-10-31 23:12:02 -0700

    Compose exercises

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -10,23 +10,15 @@ import Course.Monad
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
-newtype Compose f g a =
-  Compose (f (g a))
+newtype Compose f g a = Compose (f (g a))
 
--- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  (<$>) f (Compose fga) = Compose ((f <$>) <$> fga)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
--- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  pure = Compose . pure . pure
+  (<*>) (Compose fgf) (Compose fga) = Compose $ ((<*>) <$> fgf) <*> fga
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
